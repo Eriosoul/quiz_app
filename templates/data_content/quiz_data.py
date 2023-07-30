9bcfe394a9df990c0458e61c0fc9ab9f2c9889d0
Eriosoul <tboss1780@gmail.com>
2023-07-30 20:00:39 +0200

    A quiz app using class and data

---
+++ templates/data_content/quiz_data.py
@@ -0,0 +1,12 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "Madrid", "Bucuresti", "Moscu"],
+        "answer": "Paris"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "choices": ["Go","AU", "AG", "GD"],
+        "answer": "Paris"
+    },
+]
